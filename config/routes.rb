3ab6bf1c0df2c875dae16ed1254079ef28bd23ee
Mel Chang <mel510@users.noreply.github.com>
2017-09-19 13:13:22 -0700

    Added css, nav, and sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'application#index'
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  resources :cats
+  resources :tricks
+  resources :cat_tricks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "Mel", email: "test@test.com", password: "123456")
+
+cat1 = Cat.create(name: "Hiboux", color: "Tortoiseshell")
+cat2 = Cat.create(name: "Pigeon", color: "Gray tabby")
+
+trick1 = Trick.create(name: "Sit")
+trick2 = Trick.create(name: "Come")
+trick3 = Trick.create(name: "High five")
+trick4 = Trick.create(name: "Ring bell")
+
+CatTrick.create(cat_id: 1, trick_id: 1)
+CatTrick.create(cat_id: 1, trick_id: 2)
+CatTrick.create(cat_id: 1, trick_id: 4)
+CatTrick.create(cat_id: 2, trick_id: 2)
