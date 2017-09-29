d853ca6c5a5587f97e4bae7a48a6092210035b85
Mel Chang <mel510@users.noreply.github.com>
2017-09-29 15:43:46 -0700

    Fix nested attribute

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -6,10 +6,11 @@ class Cat < ApplicationRecord
   validates :name, presence: true
 
   def tricks_attributes=(trick_attributes)
-    #raise params.inspect
-    trick_attributes.values.each do |trick_attribute|
-      trick = Trick.find_or_create_by(trick_attribute)
-      self.tricks << trick
+    if trick_attributes["0"]["name"].length > 0
+      trick_attributes.values.each do |trick_attribute|
+        trick = Trick.find_or_create_by(trick_attribute)
+        self.tricks << trick
+      end
     end
   end
 
