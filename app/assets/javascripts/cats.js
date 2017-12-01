0a594818fe0413c871d2b52f744fd87589faa4a5
Mel Chang <mel510@users.noreply.github.com>
2017-11-30 20:55:40 -0800

    Begin implementation of previous feature on Cat show page

--- app/assets/javascripts/cats.js
+++ app/assets/javascripts/cats.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   listCats();
   nextCat();
+  previousCat();
 })
 
 
@@ -9,13 +10,13 @@ function listCats () {
   $.getJSON("/cats", function(cats){
     $.each(cats, function(index, cat) {
       $("#cat_list").append(
-        "<li>" +
-        "<a href=\"cats/" +
-        cat.id +
-        "\">" +
-        cat.name +
-        "</a>" +
-        "</li>")
+                            "<li>" +
+                            "<a href=\"cats/" +
+                            cat.id +
+                            "\">" +
+                            cat.name +
+                            "</a>" +
+                            "</li>")
     })
   })
 }
@@ -30,21 +31,37 @@ function nextCat() {
         return element.id;
       }).indexOf(currentCatId);
       var nextCat = cats[currentIndex+1];
-      console.log(nextCat);
       $(".cat-name").text(nextCat.name);
       $(".cat-color").text(nextCat.color);
       $(".js-next").attr("data-id", nextCat.id);
+
+      if (nextCat === cats[cats.length - 1]) {
+        $(".js-previous").attr("data-id", nextCat.id);
+      } else {
+        $(".js-previous").attr("data-id", currentCatId);
+      }
     })
   })
 }
-// function nextCat() {
-//   $(".js-next").on("click", function() {
-//     var currentId = parseInt($(".js-next").attr("data-id"))
-//     $.getJSON("/cats", function (cats) {
-//       $.each(cats, function(i, cat) {
-//         var prevId = cats[i-1]
-//         console.log(prevId)
-//       })
-//     })
-//   })
-// }
+
+// Cat show page: Previous
+function previousCat() {
+  $(".js-previous").on("click", function() {
+    var currentCatId = parseInt($(".js-previous").attr("data-id"));
+    $.getJSON("/cats", function(cats){
+      var currentIndex = cats.map(function(element) {
+        return element.id;
+      }).indexOf(currentCatId);
+      var previousCat = cats[currentIndex-1];
+      $(".cat-name").text(previousCat.name);
+      $(".cat-color").text(previousCat.color);
+      $(".js-previous").attr("data-id", previousCat.id);
+
+      if (previousCat === cats[0]) {
+        $(".js-next").attr("data-id", previousCat.id);
+      } else {
+        $(".js-next").attr("data-id", currentCatId);
+      }
+    })
+  })
+}
